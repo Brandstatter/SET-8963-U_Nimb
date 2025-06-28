8dc53bc022d5697e15ed838f99864f94a46b663b
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-06-28 17:12:16 -0300

    feat: search magic with dynamic autocomplete

--- src/core/magic/command.py
+++ src/core/magic/command.py
@@ -1,18 +1,39 @@
+import discord
 import random
 
 from clientConfig import client
-from core.magic.service import embed_magic, search_magic
+from core.magic.service import embed_magic, search_magic, magicAutoComplete
 
 # Random Magic
 @client.command(aliases = ['l', 'random']) 
 async def randomMagic(ctx):
-    embed = await embed_magic(random.randrange(0, 197))
+    embed = await embed_magic(random.randrange(0, 219))
     await ctx.send(embed = embed)
- 
-# Search Magic
-@client.command(aliases = ['e', 'magia']) 
-async def searchMagic(ctx):
-    id = await search_magic(ctx)
-    for magic in id:
-        embed = await embed_magic(magic)
-        await ctx.send(embed = embed)
+
+@client.slash_command(
+    name="random_magic",
+    description="Busca uma magia aleatoriamente",
+    guild_ids=[563153398392684554]
+) 
+async def random_magic_slash_command(ctx):
+    embed = await embed_magic(random.randrange(0, 219))
+    return await ctx.respond(embed = embed)
+    
+
+@client.slash_command(
+    name = "searchmagic",
+    description="Procura uma magia",    
+)
+async def searchMagicSlashCommand(
+    ctx,
+    magic_option: str = discord.Option(
+        description="Escolha a magia",
+        autocomplete=magicAutoComplete,
+    )
+):
+    id = await search_magic(magic_option)
+    if(id == None):
+        return await ctx.respond("Magia não existe")
+
+    embed = await embed_magic(id)
+    return await ctx.respond(embed = embed)
