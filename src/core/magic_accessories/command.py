9dfc0601e261108eec6c7e898cacd078d69a92db
Brandstatter <brandstatter1705@gmail.com>
2025-07-12 17:22:49 -0300

    feat: added function to get a accessory based on chosen category

--- src/core/magic_accessories/command.py
+++ src/core/magic_accessories/command.py
@@ -1,7 +1,7 @@
 import discord
 
 from clientConfig import client
-from core.magic_accessories.service import search_acessories, embed_accessory, acessory_autocomplete
+from core.magic_accessories.service import search_accessories, embed_accessory, accessory_autocomplete, roll_accessory
 
 @client.slash_command(
     name="busca_acessorio",
@@ -12,13 +12,33 @@ async def search_accessory_slash_command(
     ctx,
     acessorio_option: str = discord.Option(
         description="Escolha um acessorio",
-        autocomplete=acessory_autocomplete,
+        autocomplete=accessory_autocomplete,
     )
 ):
-    id = await search_acessories(acessorio_option)
+    id = await search_accessories(acessorio_option)
     if(id == None):
         return await ctx.respond("Esse acessório não existe")
 
     embed = await embed_accessory(id)
     return await ctx.respond(embed = embed)
-    
+
+@client.slash_command(
+    name="rolar_acessorio",
+    description="Busca um acessorio magico.",
+    guilds = [563153398392684554]
+) 
+async def search_accessory_slash_command(
+    ctx,
+    categoria: str = discord.Option(str, choices = ["Acessórios Menores", "Acessórios Médios", "Acessórios Maiores"],
+        description="Escolha categoria de acessórios que deseja."
+    )
+):
+    if categoria == "Acessórios Menores":
+        embed = await roll_accessory(0)
+        await ctx.respond(embed = embed)
+    elif categoria == "Acessórios Médios":
+        embed = await roll_accessory(1)
+        await ctx.respond(embed = embed)
+    else:
+        embed = await roll_accessory(2)
+        await ctx.respond(embed = embed)
