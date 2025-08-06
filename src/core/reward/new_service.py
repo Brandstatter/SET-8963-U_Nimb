00aee0b71a510a3a77d1ff2d4abb1d211ab8bcd3
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-08-06 20:27:53 -0300

    fix: remove fixed d100

--- src/core/reward/new_service.py
+++ src/core/reward/new_service.py
@@ -11,8 +11,7 @@ WEALTH_JSON = json.load(open(os.path.join("json", "treasure_reward.json"), encod
 
 async def get_treasure(id):
     ndObject = next((obj for obj in REWARD_JSON if obj["nd"] == id), None)
-    # d100 = random.randrange(1, 100)
-    d100 = 100
+    d100 = random.randrange(1, 100)
     if not ndObject:
         return None
 
