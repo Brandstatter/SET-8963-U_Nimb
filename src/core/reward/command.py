863f8d1d55a275cc5013ad47ec81e65cf9bd6813
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-09-16 15:39:41 -0300

    add: pagination and all rolls

--- src/core/reward/command.py
+++ src/core/reward/command.py
@@ -3,6 +3,7 @@ import discord
 from clientConfig import client
 from core.reward.service import get_percent
 from core.reward.new_service import get_treasure, embed_fail
+from core.reward.Paginator import Paginator
 
 options = ['1/4', '1/2', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
 
@@ -15,6 +16,10 @@ async def slash_money(ctx: discord.ApplicationContext,
 ):
     
     reward = await get_treasure(nd)
+    if(isinstance(reward, list)):
+        view = Paginator(reward)
+        return await ctx.respond(embed=reward[len(reward) - 1], view=view)
+    
     return await ctx.respond(embed = reward)
 
     # embed = await get_percent(options.index(nd))
--- src/core/reward/embeds/embed_wealth.py
+++ src/core/reward/embeds/embed_wealth.py
@@ -3,6 +3,7 @@ import discord
 from core.reward.treasure_enum import TreasureEnum
 
 def embed_wealth(choseReward, treasures, rolls):
+    embeds = []
     wealthType = choseReward["description"]["wealth"]["type"]
 
     embed = discord.Embed(
@@ -14,11 +15,42 @@ def embed_wealth(choseReward, treasures, rolls):
     embed.add_field(name="Número de riquezas", value=rolls, inline=False)
 
     totalSum = 0
-    for _, item in enumerate(treasures):
+    for index, item in enumerate(treasures):
         bonus = item["bonus"]
         totalSum += sum(item["rolls"]) + bonus
 
+        embeds.append(rollsEmbed(index=index, treasure=item))
+
     
     embed.add_field(name="Total das jogadas + bônus", value=f"{totalSum}", inline=False)
+    embeds.append(embed)
+
+    return embeds
+
+def rollsEmbed(index, treasure):
+    dice = treasure["dice"]
+    numberOfRolls = treasure["numberOfRolls"]
+    bonus = treasure["bonus"]
+    rolls = treasure["rolls"]
+    examples = treasure["examples"]
+    
+    rollsStr = " + ".join(str(r) for r in rolls)
+    examplesString = "\n".join(str(item) for item in examples)
+
+    totalSum = sum(rolls)
+    equation = f"d{dice}x{numberOfRolls} -> {rollsStr} = {totalSum} * {bonus} = {totalSum + bonus}" 
+
+    embed = discord.Embed(
+        title=f"Jogada: {index}",
+        description=f"{equation}",
+        color=discord.Color.random()
+    )
+
+    embed.add_field(name="Dados lançados", value=rollsStr, inline=False)
+    embed.add_field(name="Bônus", value=bonus, inline=False)
+    
+
+    embed.add_field(name="Exemplos de prêmios", value=examplesString, inline=False)
 
     return embed
+
