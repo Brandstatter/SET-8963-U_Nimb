b27d5778092ef538c3040aa19ecd135143ddea68
Brandstatter <brandstatter1705@gmail.com>
2025-09-28 16:46:52 -0300

    feat: add multiple potions roll

--- src/core/potions/command.py
+++ src/core/potions/command.py
@@ -4,9 +4,12 @@ from core.potions.service import get_potion
 
 @client.slash_command(
     name = "rolar_pocao",
-    description = "Retorna recompensa de dinheiro de acordo com o ND.",
-    
+    description = "Retorna recompensa de dinheiro de acordo com o ND."    
 )
-async def slash_potion(ctx):
-    embed = await get_potion()
+async def slash_potion(ctx,
+    quantidade : int
+                       ):
+    if quantidade > 10:
+        await ctx.respond("Limite de criação de poções execedido. Limite maximo: 10 poções")
+    embed = await get_potion(quantidade)
     await ctx.respond(embed = embed)
--- src/core/potions/service.py
+++ src/core/potions/service.py
@@ -2,6 +2,7 @@ import os
 import discord
 import json
 import random
+import asyncio
 
 json_path = os.path.join("json", "potions.json")
 POTION_JSON = json.load(open(json_path, encoding='utf-8'))
@@ -9,25 +10,33 @@ POTION_JSON = json.load(open(json_path, encoding='utf-8'))
 json_path = os.path.join("json", "potions_table.json")
 TABLE_JSON = json.load(open(json_path, encoding='utf-8'))
 
-async def get_potion():
+async def get_potion(qtd):
 
-    d100 = random.randrange(1, 100)
+    potion_ids = []
+    x = 0
 
-    for item in TABLE_JSON:
+    while x < qtd:
+        d100 = random.randrange(1, 100)
+        for item in TABLE_JSON:
             if d100 <= item['cutoffValue']:
-                embed = await embed_potion(item['rewardId'])
+                potion_ids.append(item['rewardId'])
                 break 
-
+        x = x + 1    
+    embed = await embed_potion(potion_ids)
     return embed
 
-async def embed_potion(id):
-
-    name = POTION_JSON[id]["name"]
-    description = f'{POTION_JSON[id]["desc"]} \n\n Valor: {POTION_JSON[id]["cost"]}'
+async def embed_potion(ids):
+    ids = [4, 4, 4, 4]
+    name = "Poções encontradas"
 
     embed = discord.Embed(
-    title = name,
-    description = description,
-    color=discord.Color.random())
+        title = name,
+        description="",
+        color=discord.Color.random()
+    )
+
+    for id in ids:
+        print(id)
+        embed.add_field(name=f"{POTION_JSON[id]['name']}", value=f"{POTION_JSON[id]['desc']}\n **Valor**: {POTION_JSON[id]['cost']}", inline=False)
 
     return embed
