606a68333cdcdea486219e75661a04f690999d72
Brandstatter <brandstatter1705@gmail.com>
2025-10-08 16:02:20 -0300

    feat: add esoteric generation function

--- src/core/equipament/command.py
+++ src/core/equipament/command.py
@@ -1,6 +1,7 @@
 from clientConfig import client
 from core.equipament.armor.service import get_armor
 from core.equipament.weapon.service import get_weapon
+from core.equipament.esoteric.service import get_esoteric
 
 @client.slash_command(
     name = "gerar_armadura",
@@ -22,4 +23,15 @@ async def weapons(
     ctx
 ):
     embed = await get_weapon()
+    return await ctx.respond(embed = embed)
+
+@client.slash_command(
+    name = "gerar_esoterico",
+    description = "Gera um item esoterico com base na 'Tabela 8-4: Equipamento' de Tormenta20",
+    guild_ids=[563153398392684554] 
+)
+async def esoteric(
+    ctx
+):
+    embed = await get_esoteric()
     return await ctx.respond(embed = embed)
